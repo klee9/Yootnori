53a82b5bbdbfadf0647f2e02586437e4d52b3555
JIHWAN PARK <parkjihawn5@gmail.com>
2025-05-05 03:38:39 +0900

    Player, Token  다이어그램에 맞게 수정했습니다.

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,22 +1,52 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    private int id;
     private String name;
     private List<Token> tokens;
-    private boolean has_finished;
+    private int playerId;
 
-    public List<Token> getTokens() {
+    // 플레이어 생성자 플레이어 이름 ,플레이어 ID, 말의 개수, 말의 모양(type)로 생성한다.
+    public Player(String name, int playerId, int tokenCount, TokenType type) {
+        this.name = name;
+        this.playerId = playerId;
+        this.tokens = new ArrayList<>();
+        for ( int i = 0; i < tokenCount; i++){ // 말(token)을 tokens에 개수만큼 추가한다.
+            tokens.add(new Token(i, type,this));
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public List<Token> getTokens(){
         return tokens;
     }
 
-    public void resetTokens() {
-        tokens.clear();
+    public void setTokens(List<Token> tokens) {
+        this.tokens = tokens;
     }
 
-    public int getId() {
-        return id;
+    public void resetTokens() {
+        for (Token token : tokens) {
+            token.reset(); // 각 토큰 초기화
+        }
     }
+
+    public boolean hasFinished() {
+        for (Token token : tokens) {
+            if (!token.isFinished()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }

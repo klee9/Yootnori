f771440a90a0202c9d12684fe771b3f9a827f991
Keon Lee <22269190+klee9@users.noreply.github.com>
2025-05-07 13:30:07 +0900

    GameEventListener 사용하도록 수정

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -8,7 +8,7 @@ import model.Position;
 // Gets the user gestures from the UI and runs model's functions.
 // Works as a mediator between Game and UI
 
-public class GameController {
+public class GameController implements GameEventListener {
     private MainWindow mainWindow;
     private Game game;
 
@@ -21,15 +21,20 @@ public class GameController {
      * onSelectTokenCount: 플레이어가 사용할 토큰 수 설정 시 실행
      * onSelectShape: 보드 모양 설정 시 실행
      */
+    @Override
     public void onSelectPlayerCount(int playerCount) { game.setPlayerCount(playerCount); }
+    @Override
     public void onSelectTokenCount(int tokenCount) { game.setTokenCount(tokenCount); }
+    @Override
     public void onSelectShape(int shape) { game.setBoardShape(shape); }
 
     /* 윷 관련 로직
      * onRandomThrow(): 랜덤 윷 던지기 버튼 클릭 시 실행
      * onSpecifiedThrow(): 지정 윷 던지기 버튼 클릭 시 실행
      */
+    @Override
     public void onRandomThrow() { game.randomThrow(); }
+    @Override
     public void onSpecifiedThrow(int throwResult) { game.specifiedThrow(throwResult); }
 
     /* 말 관련 로직
@@ -39,17 +44,24 @@ public class GameController {
      * onStackTokensYes: 말 업겠다고 하면 실행
      * onCaptureTokensYes: 말 잡겠다고 하면 실행
      */
+    @Override
     public void onClickPosition(int positionIndex) { game.selectPosition(positionIndex); } // TODO: 눌렀을 때 어떤 행동을 해야 하는지 판단
+    @Override
     public void onClickToken(int tokenIndex) { game.selectToken(tokenIndex); }
-    public void onMoveTokens(Token token, Position newPos, int throwResult) { game.applyMoveTo(token, newPos, throwResult); }
+    @Override
+    public void onMoveTokens(Token token, Position destination, int throwResult) { game.applyMoveTo(token, destination, throwResult); }
+    @Override
     public void onStackTokens(Token tokenA, Token tokenB) { game.handleStacking(tokenA, tokenB); }
-    public void onCaptureTokensYes(Token tokenA, Token tokenB) { game.handleCapturing(tokenA, tokenB); }
+    @Override
+    public void onConfirmCaptureTokens(Token tokenA, Token tokenB) { game.handleCapturing(tokenA, tokenB); }
 
     /* 게임 흐름 로직
-     * onExitYes(): 게임 종료 버튼 누르면 실행 
+     * onExitYes(): 게임 종료 버튼 누르면 실행
      * onRestartYes(): 재시작 버튼 누르면 실행
      */
-    public void onExitYes() { game.endGame(); } // TODO: 게임 종료 거절 버튼을 누르면 UI상에서 뒤로가기만 수행하면 될 것 같아요
-    public void onRestartYes() { game.restartGame(); }
+    @Override
+    public void onConfirmExit() { game.endGame(); } // TODO: 게임 종료 거절 버튼을 누르면 UI상에서 뒤로가기만 수행하면 될 것 같아요
+    @Override
+    public void onConfirmRestart() { game.restartGame(); }
 
 }

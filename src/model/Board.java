d35fc5b2b9aed02a36f6b777037a371c8b1c9a23
Keon Lee <22269190+klee9@users.noreply.github.com>
2025-05-07 20:41:52 +0900

    변수 이름, 타입 통일

--- src/model/Board.java
+++ src/model/Board.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-    private int shapeType;
+    private String shapeType;
     private List<Position> positions;
 
-    public Board(int shapeType) {
+    public Board(String shapeType) {
         this.shapeType = shapeType;
         this.positions = new ArrayList<>();
 
@@ -15,23 +15,28 @@ public class Board {
         initBoard(shapeType);
     }
 
-    public void initBoard(int shape) {
+    public void initBoard(String shape) {
         // add positions
-        for (int i = 0; i < 7*shape + 1; i++) {
-            positions.add(new Position(i, shape));
+        int numCorners = 0;
+        switch (shape) {
+            case "사각형" -> numCorners = 4;
+            case "오각형" -> numCorners = 5;
+            case "육각형" -> numCorners = 6;
+        }
+        for (int i = 0; i < 7*numCorners + 1; i++) {
+            positions.add(new Position(i, numCorners));
         }
 
         // add connections (prev, next positions)
         int[][] connections = null;
-        int square = 4, pentagon = 5, hexagon = 6;
-        if (shape == square) {
+        if (shape.equals("사각형")) {
             connections = new int[][] {
                     {0,1}, {1,2}, {2,3}, {3,4}, {4,5}, {5,6}, {5,22}, {6,7}, {7,8}, {8,9}, {9,10}, {10,11}, {10,24}, {11,12}, {12,13}, {13,14}, {14,15},
                     {15,16}, {16,17}, {17,18}, {18,19}, {19,0}, {20,0}, {21,20}, {22,23}, {23,28}, {24,25}, {25,28}, {26,15}, {27,26}, {28,21}, {28, 27}
             };
         }
 
-        else if (shape == pentagon) {
+        else if (shape.equals("오각형")) {
             connections = new int[][] {
                     {0,1}, {1,2}, {2,3}, {3,4}, {4,5}, {5,6}, {5,27}, {6,7}, {7,8}, {8,9}, {9,10}, {10,11}, {10,29}, {11,12}, {12,13},
                     {13,14}, {14,15}, {15,16}, {15,31}, {16,17}, {17,18}, {18,19}, {19,20}, {20,21}, {20,33}, {21,22}, {22,23}, {23,24},
@@ -39,7 +44,7 @@ public class Board {
             };
         }
 
-        else if (shape == hexagon) {
+        else if (shape.equals("육각형")) {
             connections = new int[][] {
                     {0,1}, {1,2}, {2,3}, {3,4}, {4,5}, {5,6}, {5,32}, {6,7}, {7,8}, {8,9}, {9,10}, {10,11}, {10,34}, {11,12}, {12,13}, {13,14},
                     {14,15}, {15,16}, {15,36}, {16,17}, {17,18}, {18,19}, {19,20}, {20,21}, {20,38}, {21,22}, {22,23}, {23,24}, {24,25}, {25,26},

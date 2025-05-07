18c1e808b09d02850e2b6b81a653ca3abd087584
Keon Lee <22269190+klee9@users.noreply.github.com>
2025-05-07 20:44:24 +0900

    불필요한 함수 제거

--- src/model/RuleSet.java
+++ src/model/RuleSet.java
@@ -3,17 +3,9 @@ package model;
 import java.util.List;
 
 public class RuleSet {
-    public boolean getAdditionalTurn(int throwResult, int  moveResult) {
-        return ((throwResult == 4 || throwResult == 5) && moveResult != 7) || (throwResult < 4 && moveResult == 7); // -1~5: 윷 던지기 결과, 6: 업기, 7: 잡기라고 가정
-    }
-
-    public boolean checkToss(int tossResult) {
-        return tossResult >= -1 && tossResult <= 5;
-    }
-
-    public boolean checkMove(Position currPos, Position nextPos, int tossResult) {
+    public boolean checkMove(Position currPos, Position nextPos, TossResult tossResult) {
         // 백도
-        if (tossResult == -1) {
+        if (tossResult == TossResult.BACKDO) {
             return currPos.getPrevPositions().contains(nextPos);
         }
 
@@ -25,7 +17,7 @@ public class RuleSet {
             }
 
             for (Position p : nextPositions) {
-                for (int i = 1; i < tossResult; i++) {
+                for (int i = 1; i < tossResult.getValue(); i++) {
                     p = p.getNextPositions().get(0);
                     if (p.isGoal()) {
                         return true;
